ae83f413dee05ef693a22c2b47252b8524e9b335
klhsu <allen.klhsu@gmail.com>
2021-09-29 22:57:12 +0800

    function to download transcript genbank file

---
+++ NCBI_downloader.py
@@ -0,0 +1,24 @@
+import os
+from Bio import Entrez
+
+def NCBI_downloader(query_ls, folder):
+
+    Entrez.email = "d0789100@gmail.com"  # Always tell NCBI who you are
+    files = [file for file in os.listdir(folder)]
+    
+    
+    for NM_ in query_ls: 
+        file = NM_ + '.gb'
+        filename = folder + file
+        try:
+            if file not in files:
+                # Downloading...
+                input_handle = Entrez.efetch(
+                    db="nucleotide", id=NM_, rettype="gb", retmode="text"
+                )
+                out_handle = open(filename, "w")
+                out_handle.write(input_handle.read())
+                out_handle.close()
+                input_handle.close()
+        except:
+            print("Interrupted!!There are some problems when handling "+ NM_)
